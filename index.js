09ba7d8160ab45097d6ef3731715fd3ad2708941
Alisse Frandsen <alisse.frandsen@gmail.com>
2018-10-17 00:32:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 /* Enter the code to remove the main node element under this comment */
-
+main.remove()
+  
 /* Create your new element here and assign it to newHeader */
-const newHeader = null;
+const header = document.createElement('h1')
+header.id = 'victory'
+header.innerText = "Alisse is the champion!"
+document.body.appendChild(header)
+const newHeader = document.querySelector('#victory');
+
+ 
