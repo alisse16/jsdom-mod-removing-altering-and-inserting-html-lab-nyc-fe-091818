e672dc5c0ec413d07236c337c3403fce7b3100f7
Alisse Frandsen <alisse.frandsen@gmail.com>
2018-10-17 00:22:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 /* Enter the code to remove the main node element under this comment */
-
+element.remove('#main')
+  
 /* Create your new element here and assign it to newHeader */
 const newHeader = null;
+let 'victory' = document.createElement('h1')
+.victory.innerText = 'Alisse is the champion!'
+ 
